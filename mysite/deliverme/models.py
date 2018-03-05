1843ca51da1ae768c92ee6cf19883ea523583e83
TVTjannel <daniel.tjandra96@gmail.com>
2018-03-05 15:57:53 -0800

    migrations

--- mysite/deliverme/models.py
+++ mysite/deliverme/models.py
@@ -1,6 +1,14 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
-# Create your models here.
+class Post(models.Model):
+    title = models.TextField()
+    user =  models.TextField()
+    message = models.TextField()
+    #location =
+    #destination =
+    time = models.DateTimeField() #%Y-%m-%d %H:%M:%S
+    requester = models.NullBooleanField() #True if requester, false if deliverer, allowing null for testing currently
+
+    def __str__(self):
+        return self.title
