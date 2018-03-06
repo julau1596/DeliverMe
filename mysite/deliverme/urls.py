d0fb4abf701bca2a2049a4d7424bdf7c1c5f8042
jefrayc@gmail.com <jefrayc@gmail.com>
2018-03-06 00:46:24 -0800

    log in system all done

--- mysite/deliverme/urls.py
+++ mysite/deliverme/urls.py
@@ -7,7 +7,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name = 'index'),
     url(r'^login/$', auth_views.login, {'template_name': 'registration/login.html'}, name='login'),
-    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^login/home/$', views.home),
     url(r'^signup/$', views.signup, name='signup'),
 
