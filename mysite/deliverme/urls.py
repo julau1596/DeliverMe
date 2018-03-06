a70da14e2d05b4403000505e0332e10078f9d2bc
TVTjannel <daniel.tjandra96@gmail.com>
2018-03-06 00:27:22 -0800

    testing urlpatterns

--- mysite/deliverme/urls.py
+++ mysite/deliverme/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url, include
-#from . import views
+from . import views
 from django.views.generic import ListView, DetailView
 from deliverme.models import Post
 
 urlpatterns = [
     #Fetches Post objects, latest 50 posts sorted by date/time, puts them into main.html
-    url(r'^$', ListView.as_view(queryset=Post.objects.all().order_by("-time")[:50], template_name="deliverme/main.html"))
+    #url(r'^$', ListView.as_view(queryset=Post.objects.all().order_by("-time")[:50], template_name="deliverme/main.html")),
+    url(r'^$', views.index, name = 'index'),
 ]
