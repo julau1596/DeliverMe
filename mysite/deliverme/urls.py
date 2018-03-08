838152f6fe639a922c14fee792b80d2607dd60b6
TVTjannel <daniel.tjandra96@gmail.com>
2018-03-08 00:43:51 -0800

    populating columns

--- mysite/deliverme/urls.py
+++ mysite/deliverme/urls.py
@@ -8,7 +8,7 @@ from deliverme.models import Post
 
 urlpatterns = [
     #Fetches Post objects, latest 50 posts sorted by date/time, puts them into main.html
-    #url(r'^$', ListView.as_view(queryset=Post.objects.all().order_by("-time")[:50], template_name="deliverme/main.html")),
+    #url(r'^list/$', ListView.as_view(queryset=Post.objects.all().order_by("-time")[:50], template_name="deliverme/main.html")),
     url(r'^$', views.index, name = 'index'),
     url(r'^login/$', auth_views.login, {'template_name': 'registration/login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
