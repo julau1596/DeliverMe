09f8054cd51124b7db973252905c5a79b2d69d28
TVTjannel <daniel.tjandra96@gmail.com>
2018-03-15 02:11:06 -0700

    restructured model for Post table

--- mysite/deliverme/models.py
+++ mysite/deliverme/models.py
@@ -4,9 +4,10 @@ from django.utils.encoding import python_2_unicode_compatible
 class Post(models.Model):
     title = models.TextField()
     user =  models.TextField()
-    message = models.TextField()
-    #location =
-    #destination =
+    locLat = models.FloatField(blank=True, null=True)
+    locLon = models.FloatField(blank=True, null=True)
+    destLat = models.FloatField(blank=True, null=True)
+    destLon = models.FloatField(blank=True, null=True)
     time = models.DateTimeField() #%Y-%m-%d %H:%M:%S
     requester = models.NullBooleanField() #True if requester, false if deliverer, allowing null for testing currently
 
